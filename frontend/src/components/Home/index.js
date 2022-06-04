8484f21cc7f7ab4c5dffc2c80a95f6f3d8ee4b63
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2022-06-04 19:19:58 -0400

    Fixed splash page and link

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,21 +1,23 @@
 import './Home.css';
-import { NavLink } from 'react-router-dom';
 
 const Home = () => {
 	return (
 		<div className="splashPage">
-			<div className="introLetters">
-				<h1 className="welcome">Welcome to Kuora</h1>
-				<h2>
-					Ask whatever you want.
-					<br />
-					Discover questions and answers.
-					<br />
-					Share your knowledge.
-				</h2>
-			</div>
+			<h1 className="welcome">
+				Welcome to <span className="letterLogo">Kuora</span>
+			</h1>
+			<h2 className="mantra">
+				Ask whatever you want.
+				<br />
+				Discover questions and answers.
+				<br />
+				Share your knowledge.
+			</h2>
 			<div className="splashImage">
-				<img className="imageSplash" src="https://images.unsplash.com/photo-1652932420117-0055d554e60f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80" />
+				<img
+					className="imageSplash"
+					src="https://images.unsplash.com/photo-1588594276800-2de0522b3b73?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
+				/>
 			</div>
 		</div>
 	);
